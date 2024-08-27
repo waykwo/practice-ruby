f98224d91a235f6a4478b4043c0dc3aa34e74824
waykwo <72991933+waykwo@users.noreply.github.com>
2024-08-26 22:30:15 -0400

    Add store_items

---
+++ store_itme.rb
@@ -0,0 +1,14 @@
+inventory = [
+  {:item_name => "gummy worms", colour: "multi", price: 0.25},
+  {:item_name => "hot lips", colour: "red", price: 0.35},
+  {:item_name => "gummy bears", colour: "multi", price: 0.10},
+]
+pp inventory
+
+gummy_worms = {:colour => "multi", price: 0.25}
+hot_lips = {:colour => "red", price: 0.35}
+gummy_bears = {:colour => "multi", price: 0.10}
+
+p gummy_worms
+p hot_lips
+p gummy_bears
