45809ad447aecb713c54c82b4e85866e72ae70f9
waykwo <72991933+waykwo@users.noreply.github.com>
2024-08-27 20:51:24 -0400

    Add manager.rb exercise

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,4 +1,10 @@
-# class Car
+# • Use the terminal to create a new file in your practice-ruby folder called inheritance_example.rb and open it in your text editor.
+# • Copy the Car and Bicycle class into your file: https://gist.github.com/peterxjang/e9b69b4de36b2c41ef4b
+# • Use inheritance to DRY up the classes. Note - a car is NOT in a type of bicycle, and a bicycle is NOT a type of car!
+# • To test it, create a bicycle and car instance from your newly modified classes (you can create them at the bottom of your file). First, have them accelerate. Then, make sure a bike can “Ring ring!” and a car “Beeeeeeep!”
+# Bonus: Read more about inheritance and object hierarchy in Ruby here: https://www.leighhalliday.com/object-hierarchy-in-ruby
+
+# # class Car
 #   def initialize
 #     @speed = 0
 #     @direction = 'north'
